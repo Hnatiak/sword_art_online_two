d4465ae0c0e940b84dc79215cea37112aa721ef9
Hnatiak <58102343+Hnatiak@users.noreply.github.com>
2023-08-29 16:16:51 +0200

    I correxted a index.html

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,53 +1,3 @@
-// import React from 'react';
-// import {
-//   Container,
-//   MenuList,
-//   MenuItem,
-//   Border,
-// } from './Menu.styled';  // Попередньо визначені стилі для компонента
-// import { faUser, faUserGroup, faComments, faMapLocationDot, faGear } from '@fortawesome/free-solid-svg-icons';
-// import hoverSound from '../sounds/SAO_Menu_SFX.mp3';
-
-
-// const Menu = () => {
-//     const audio = new Audio(hoverSound);
-
-//   const playHoverSound = () => {
-//     audio.play();
-//   };
-
-//   const handleMenuItemMouseEnter = () => {
-//     playHoverSound();
-//   };
-
-//   return (
-//     <Container>
-//       <MenuList>
-//         <Border onMouseEnter={handleMenuItemMouseEnter}>
-//             <MenuItem icon={faUser} data-isfirstorlast="true" />
-//         </Border>
-//         <Border onMouseEnter={handleMenuItemMouseEnter}>
-//             <MenuItem icon={faUserGroup} />
-//         </Border>
-//         <Border onMouseEnter={handleMenuItemMouseEnter}>
-//             <MenuItem icon={faComments} />
-//         </Border>
-//         <Border onMouseEnter={handleMenuItemMouseEnter}>
-//             <MenuItem icon={faMapLocationDot} />
-//         </Border>
-//         <Border onMouseEnter={handleMenuItemMouseEnter}>
-//             <MenuItem icon={faGear} data-isfirstorlast="true" />
-//         </Border>
-//       </MenuList>
-//     </Container>
-//   );
-// };
-
-// export default Menu;
-
-
-// щудшаруа
-
 import React, { useState } from 'react'; // { useState } 
 import {
   Container,
@@ -77,11 +27,9 @@ const Menu = () => {
   };
 
   return (
-    <Container onClick={handlePageClick}> 
-        {/* onClick={handlePageClick} */}
+    <Container onClick={handlePageClick}>
       <MenuList>
         <Border onClick={handleMenuItemMouseEnter}>
-            {/* onClick={handleMenuItemMouseEnter}> */}
             <MenuItem icon={faUser} data-isfirstorlast="true" />
         </Border>
             <Border onClick={handleMenuItemMouseEnter}>
